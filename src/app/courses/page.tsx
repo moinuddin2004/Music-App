e7f131e5271497be0c90d0cb1de39e3428ed5689
moin uddin <s.m.moin2004@gmail.com>
2024-03-05 22:28:57 +0500

    all UI done

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -28,7 +28,7 @@ function page() {
           {courseData.courses.map((course:course) => {
               return (
                   
-                  <CardContainer className="inter-var">
+                  <CardContainer key={course.title} className="inter-var">
                       <BackgroundGradient className="rounded-[22px]">
                 <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                   <CardItem
